671f9c4686987f2d52fa6b6c41670071b266f398
Dominik Mack <dominik.mack@icloud.com>
2020-08-06 17:51:03 +0200

    Removed App- and SceneDelegate, since they are not necessary in iOS 14 anymore.

---
+++ pr0gramm/pr0gramm.swift
@@ -0,0 +1,21 @@
+//
+//  pr0gramm.swift
+//  pr0gramm
+//
+//  Created by Dominik Mack on 06.08.20.
+//  Copyright © 2020 Doctor. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+@main
+struct pr0gramm: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    
+}
